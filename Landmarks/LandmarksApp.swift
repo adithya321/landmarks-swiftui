fa2044dbcba28710b94f9e99edf010bea7ccded4
Adithya Jayasankar <adithya.j@adithya321.me>
2022-12-04 16:41:37 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Adithya Jayasankar on 04/12/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
