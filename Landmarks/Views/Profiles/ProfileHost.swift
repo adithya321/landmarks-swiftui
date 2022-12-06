bee354652ac06ee3af5e50fb6f4457e6fb8c7897
Adithya Jayasankar <adithya.j@adithya321.me>
2022-12-06 07:56:06 +0530

    feat: display a user profile

---
+++ Landmarks/Views/Profiles/ProfileHost.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileHost.swift
+//  Landmarks
+//
+//  Created by Adithya Jayasankar on 06/12/22.
+//
+
+import SwiftUI
+
+struct ProfileHost: View {
+    @State private var draftProfile = Profile.default
+    
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            ProfileSummary(profile: draftProfile)
+        }
+        .padding()
+    }
+}
+
+struct ProfileHost_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileHost()
+    }
+}
