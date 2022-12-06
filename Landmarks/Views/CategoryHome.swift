aa58cf3640e85ede3efb86e73c73e77e0e5687f7
Adithya Jayasankar <adithya.j@adithya321.me>
2022-12-06 07:46:14 +0530

    feat: composing complex interfaces

--- Landmarks/Views/CategoryHome.swift
+++ Landmarks/Views/CategoryHome.swift
@@ -9,13 +9,21 @@ import SwiftUI
 
 struct CategoryHome: View {
     @EnvironmentObject var modelData: ModelData
-
+    
     var body: some View {
         NavigationView {
             List {
+                modelData.features[0].image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
-                    Text(key)
+                    CategoryRow(categoryName: key, items: modelData.categories[key]!)
                 }
+                .listRowInsets(EdgeInsets())
             }
             .navigationTitle("Featured")
         }
