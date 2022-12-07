e084cb12a5bf548c41234e380c593e6ddd8909f9
Adithya Jayasankar <adithya.j@adithya321.me>
2022-12-07 21:09:26 +0530

    feat(mac): add target

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Adithya Jayasankar on 07/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
