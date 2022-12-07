ff26428c71bc8fc4af46668110cbfa434dad91ba
Adithya Jayasankar <adithya.j@adithya321.me>
2022-12-07 20:38:19 +0530

    feat(watch): add target

---
+++ WatchLandmarks Watch App/WatchLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WatchLandmarksApp.swift
+//  WatchLandmarks Watch App
+//
+//  Created by Adithya Jayasankar on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct WatchLandmarks_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
